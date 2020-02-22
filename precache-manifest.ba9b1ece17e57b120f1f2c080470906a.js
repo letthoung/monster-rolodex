ee8995a87f5e36b2e207534918fb9eaec7740c6b
thanhtung3496 <33385100+thanhtung3496@users.noreply.github.com>
2020-02-22 11:31:28 -0500

    Updates

---
+++ precache-manifest.ba9b1ece17e57b120f1f2c080470906a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec275b0750ccaad40e1f56c39e7526cf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b3fb1ba86e6e2a54770",
+    "url": "/monsters-rolodex/static/css/main.c96d76d2.chunk.css"
+  },
+  {
+    "revision": "ae4467ac1b6874332135",
+    "url": "/monsters-rolodex/static/js/2.f5a471c9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.f5a471c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3fb1ba86e6e2a54770",
+    "url": "/monsters-rolodex/static/js/main.71cf4c59.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
